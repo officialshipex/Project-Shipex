e2922479eae06de41f09fe17fef365cbab9f21ef
bijaybhanja <bhanjabijayketan@gmail.com>
2024-10-15 17:47:20 +0530

    Track by Mobile Number,Track by AWB,Track by Order ID

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import NavBar from "./components/Common/NavBar";
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/hello")
+    fetch("http://localhost:5000/v1/external/auth/login")
       .then((response) => response.json())
       .then((data) => setMessage(data.message));
   }, []);
 
+
   return (
     <>
+    <NavBar/>
       <h1 className="underline">This is Frontend and </h1>
       <h1 className="font-bold">{message}</h1>
     </>
