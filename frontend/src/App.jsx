2613f702342cb6907afa2dd2b1925a21ff77d448
vivekanandapadhi <vivekanandapadhi8249@gmail.com>
2024-10-23 00:20:04 +0530

    sucessfull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,8 @@
 import "./App.css";
-<<<<<<< HEAD
-import "../src/components/Common/NavBar"
-// import LoginPage from "../src/components/Kyc/LoginPage"
-// import KycStep1 from "../src/components/Kyc/KycStep1"
-// import KycStep2 from "../src/components/Kyc/KycStep2"
-// import KycStep3 from "../src/components/Kyc/KycStep3"
-// import KycStep4 from "../src/components/Kyc/KycStep4"
-// import Agreement from "../src/components/Kyc/Agreement"
-// import ThankyouPage from "./components/Kyc/ThankyouPage"
-// import UploadId from "./components/Kyc/UploadId"
-// import Writemanually from "./components/Kyc/Writemanually"
-
-import { useState, useEffect } from "react";
-import NavBar from "./components/Common/NavBar";  
-=======
 import { useState, useEffect } from "react";
 import NavBar from "./components/Common/NavBar";
 import Dashboard from "./components/Dashboard/Dashboard";
 import { OrderDashboard } from "./components/Dashboard/OrderDashboard"; // Only import OrderDashboard
->>>>>>> 2ce6923233659eaa7e848a6f22de77e626b97c90
 
 function App() {
   const [message, setMessage] = useState("");
@@ -33,7 +17,7 @@ function App() {
     <div className="app-container">
       <NavBar />
       <Dashboard />
-      <OrderDashboard /> {/* Render OrderDashboard, which includes RtoDashboard */}
+      <OrderDashboard /> 
     </div>
   );
 }
