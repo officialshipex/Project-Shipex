89c81819b0d0fb6e4eadca8da2d90cc691018590
bijaybhanja <bhanjabijayketan@gmail.com>
2024-10-24 17:47:32 +0530

    registeration page

---
+++ frontend/src/registeraion/Registeration.jsx
@@ -0,0 +1,19 @@
+import OurOneStopSolutions from "../trackByMobileAWBOrderID/OurOneStopSolution"
+import PartnersSection from "../trackByMobileAWBOrderID/PartnersSection"
+import UniqueFeatures from "../trackByMobileAWBOrderID/UniqueFeature"
+import RegisterationForm from "./RegisterationForm"
+
+const Registeration = () => {
+    return (
+        <div>
+            <RegisterationForm />
+            <UniqueFeatures />
+            <OurOneStopSolutions />
+            <PartnersSection />
+
+        </div>
+    )
+}
+export default Registeration
+
+
