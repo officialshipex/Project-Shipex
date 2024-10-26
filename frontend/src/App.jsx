60d4d4746ab3248cf2d9a8e7cb3d7c95d7f945ca
Dactar <kulmc01@gmail.com>
2024-10-26 14:14:45 +0530

    complete login end to end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,43 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./components/Kyc/LoginPage";
+
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { useState } from "react";
+
+import Writemanually from "../src/components/Kyc/Writemanually";
+import ThankyouPage from "../src/components/Kyc/ThankyouPage";
+import Registeration from "./registeraion/Registeration";
+import Agreement from "../src/components/Kyc/Agreement";
 import KycStep1 from "../src/components/Kyc/KycStep1";
 import KycStep2 from "../src/components/Kyc/KycStep2";
 import KycStep3 from "../src/components/Kyc/KycStep3";
 import KycStep4 from "../src/components/Kyc/KycStep4";
 import UploadId from "../src/components/Kyc/UploadId";
-import Writemanually from "../src/components/Kyc/Writemanually";
-import Agreement from "../src/components/Kyc/Agreement";
-import ThankyouPage from "../src/components/Kyc/ThankyouPage";
+import LoginPage from "./components/Kyc/LoginPage";
+
+const PrivateRoute = ({ isAuthenticated }) => {
+  return isAuthenticated ?
+    <>
+      <Outlet />
+    </>
+    : <><Navigate replace to='/login'></Navigate></>
+}
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/KycStep1" element={<KycStep1 />} />
+
+        <Route path="/" element={<Registeration setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/login" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+
+        <Route path="/KycStep1" element={<PrivateRoute isAuthenticated={isAuthenticated}/>}>
+          <Route path="/KycStep1" element={<KycStep1 />} />
+        </Route>
+
         <Route path="/KycStep2" element={<KycStep2 />} />
         <Route path="/KycStep3" element={<KycStep3 />} />
         <Route path="/upload-id" element={<UploadId />} />
@@ -28,5 +49,9 @@ function App() {
     </Router>
   );
 }
+
+PrivateRoute.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+};
 
 export default App;
