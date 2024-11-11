390030b9c12ff96fb96a178207856f68f9833c40
vivekanandapadhi <vivekanandapadhi8249@gmail.com>
2024-11-11 12:19:07 +0530

    Updated sucessfully

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 
-// import Dashboard from "./components/Dashboard/DashBoard";
-// import OrderDashboard from "./components/Dashboard/OrderDashboard";
-import KycStep1 from "./components/Kyc/KycStep1";
+import Dashboard from "./components/Dashboard/DashBoard";
+import OrderDashboard from "./components/Dashboard/OrderDashboard";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <Dashboard /> */}
-    {/* <OrderDashboard /> */}
-    <KycStep1 />
+    <Dashboard />
+    <OrderDashboard />
   </StrictMode>
 );
