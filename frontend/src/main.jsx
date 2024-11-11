908b9222addd68c87a24f1c065b061731c339313
vivekanandapadhi <vivekanandapadhi8249@gmail.com>
2024-11-11 19:05:47 +0530

    update sucessfully

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,33 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import Paytm from "./payment/Paytm";
+// // import App from "./App";
+
+// // import Dashboard from "./components/Dashboard/DashBoard";
+// // import OrderDashboard from "./components/Dashboard/OrderDashboard";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     {/* <App /> */}
+//     <Paytm />
+//   </StrictMode>
+// );
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
-// import Paytm from "./payment/Paytm"
 // import App from "./App";
-
-// import Dashboard from "./components/Dashboard/DashBoard";
-// import OrderDashboard from "./components/Dashboard/OrderDashboard";
+// import MainOrder from "./components/Dashboard/MainOrder";
+import Dashboard from "./components/Dashboard/DashBoard";
+// import Support1 from "./components/Support/Support1";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    {/* <Paytm/> */}
+    {/* <App /> */}
+    {/* <MainOrder /> */}
+    <Dashboard />
+    {/* <Support1 /> */}
   </StrictMode>
 );
