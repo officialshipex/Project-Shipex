f9ff8984a383192e7a770113c82806317fa2e8eb
Kh-ushi <khushigupta31052002@gmail.com>
2024-11-12 00:01:32 +0530

    modified

--- backend/routes/getServices.router.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const servicesController=require("../services/servicesController");
-
-router.get("/",servicesController.getServices);
