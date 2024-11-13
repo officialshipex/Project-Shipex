357a3aa06561f80a1ae78e6cae6b16718d693a32
Dactar <kulmc01@gmail.com>
2024-11-13 15:58:13 +0530

    13 nov 2

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,25 @@
 const passport = require("passport");
 const express = require("express");
-const path = require("path");
+const helmet  = require('helmet');
 const cors = require("cors");
 
-const verficationRouter = require("./routes/kyc.router");
-// const shiprocket = require("./routes/courierB2C.router");
-const authRouter = require("./routes/auth.router");
-// const helmet = require("helmet");
-// const verficationRouter = require('./routes/kyc.router');
-const shiprocket =require("./routes/courierB2C.router")
-// const authRouter = require('./routes/auth.router');
-const helmet  = require('helmet');
 const courierServicesRoutes=require('./routes/courierServiceB2C.router')
-const paytmRoutes = require("./routes/paytm.router");
 const shiprocketRoutes = require('./routes/courierServiceB2C.router');
 const { isAuthorized } = require('./middleware/auth.middleware');
 const rechargeRouter = require("./recharge/recharge.route");
+const verficationRouter = require("./routes/kyc.router");
+const shiprocket =require("./routes/courierB2C.router")
+const paytmRoutes = require("./routes/paytm.router");
+const authRouter = require("./routes/auth.router");
 
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
 app.use(passport.initialize());
 
 app.use('/v1/external', authRouter);
