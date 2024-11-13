d0f1608ecdf8ca4bad52fe73052bbde47c620b10
Dactar <kulmc01@gmail.com>
2024-11-13 19:09:19 +0530

    resolve bijay conflict 1

--- backend/routes/calculateRate.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const calculateRateController=require("../Rate/calculateRateController");
-
-router.post("/b2c",calculateRateController.calculateRate);
-
-module.exports=router
--- backend/routes/saveRate.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const saveRateController=require("../Rate/saveRateCardController");
-
-router.post("/saverate",saveRateController.saveRate);
-
-module.exports=router
