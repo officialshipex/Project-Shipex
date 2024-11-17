6b78d99763deca2b8b35250daca4226aad873cf6
Kh-ushi <115355440+Kh-ushi@users.noreply.github.com>
2024-11-17 12:20:22 +0530

    NimbusPost API Integration Completed

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const getBaseRateController=require("./routes/getBaseRate.router");
 const userController=require("./routes/getUsers.router");
 const customRateController=require("./routes/saveCustomRate.router");
 const editBaseRateController=require("./routes/editBaseRate.router");
-// const NimbusPostController=require("./AllCouriersRoutes/nimbuspost.router");
+const NimbusPostController=require("./AllCouriersRoutes/nimbuspost.router");
 
 const verficationRouter = require("./routes/kyc.router");
 // const shiprocket =require("./routes/courierB2C.router")
@@ -54,7 +54,7 @@ app.use('/v1/getBaseRate',getBaseRateController);
 app.use('/v1/users',userController);
 app.use('/v1/saveCustomRate',customRateController);
 app.use('/v1/editBaseRate',editBaseRateController);
-// app.use("/v1/NimbusPost",ed)
+app.use("/v1/NimbusPost",NimbusPostController)
 
 
 
