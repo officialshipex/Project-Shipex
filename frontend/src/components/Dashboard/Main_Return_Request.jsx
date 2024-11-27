403101b1bc7be5bb43b11fb7bfcc83410d8785f2
Your Name <your-email@example.com>
2024-11-28 00:57:44 +0530

    Return pages update

--- frontend/src/components/Dashboard/Main_Return_Request.jsx
+++ frontend/src/components/Dashboard/Main_Return_Request.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import NavBar from "../Common/NavBar";
-import Sidebar from "../Common/Sidebar";
 import { FaChevronDown, FaSearch } from "react-icons/fa";
 
 // Sample order data for return requested
 const returnRequestedData = [
   {
     id: "RR123",
-    date: "2024-11-01",
+    date: "2024-11-01T05:30:00Z",
     buyerName: "John Doe",
     email: "john@example.com",
     product: "T-Shirt",
@@ -140,7 +138,7 @@ const ReturnList = () => {
   const getHeaders = (status) => {
     switch (status) {
       case "Return requested":
-        return ["Request Id & Date", "Buyer Details", "Product Details", "Return Reason", "Refund Details", "Status", "Action"];
+        return ["Request Id & Date", "Buyer Details", "Product Details", "Dimensions", "Warehouse Address", "Refund Details", "Action"];
       case "New return requested":
         return ["Request Id & Date", "Buyer Details", "Product Details", "Dimensions", "Warehouse Address", "Refund Details", "Action"];
       case "Schedule pickup":
@@ -157,122 +155,123 @@ const ReturnList = () => {
   };
 
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <NavBar />
-        <div className="container mx-auto mt-0 bg-white shadow-md rounded-lg p-2">
+    <div className="container mx-auto mt-0 bg-white rounded-lg p-2">
+      <div className="flex justify-between items-center mb-4 p-4">
+        <h2 className="text-2xl font-bold">Return Orders</h2>
+        <div className="flex items-center space-x-3">
+          <button className="px-4 py-2 bg-[#0CBB7D] text-white rounded-md text-sm font-medium hover:bg-[#0AA56F]">
+            Select all Orders
+          </button>
 
-          <div className="flex justify-between items-center mb-4 p-4">
-            <h2 className="text-2xl font-bold">Return Orders</h2>
-            <div className="flex items-center space-x-3">
-              <button className="px-4 py-2 bg-[#0CBB7D] text-white rounded-md text-sm font-medium hover:bg-[#0AA56F]">
-                Select all Orders
-              </button>
-            
-              <div className="relative">
-                <button className="flex items-center px-4 py-2 bg-gray-100 text-gray-700 rounded-md text-sm font-medium border border-gray-300 hover:bg-gray-200">
-                  Pick the dates
-                  <FaChevronDown className="ml-2" />
-                </button>
-              </div>
-              <div className="flex items-center px-4 py-2 border border-gray-300 rounded-md bg-gray-100">
-                <FaSearch className="text-gray-500 mr-2" />
-                <input 
-                  type="text" 
-                  placeholder="Search AWB number" 
-                  className="bg-transparent focus:outline-none text-sm" 
-                />
-              </div>
-            </div>
+          <div className="relative">
+            <button className="flex items-center px-4 py-2 bg-gray-100 text-gray-700 rounded-md text-sm font-medium border border-gray-300 hover:bg-gray-200">
+              Pick the dates
+              <FaChevronDown className="ml-2" />
+            </button>
           </div>
+          <div className="flex items-center px-4 py-2 border border-gray-300 rounded-md bg-gray-100">
+            <FaSearch className="text-gray-500 mr-2" />
+            <input
+              type="text"
+              placeholder="Search AWB number"
+              className="bg-transparent focus:outline-none text-sm"
+            />
+          </div>
+        </div>
+      </div>
 
-          <div className="flex items-center mb-4">
-            {["Return requested", "New return requested", "Schedule pickup", "In transit", "Received", "Pending refund", "All"].map((status, index) => (
+      <div className="relative border-b mb-4">
+        <div className="flex items-center space-x-6">
+          {[
+            "Return requested",
+            "New return requested",
+            "Schedule pickup",
+            "In transit",
+            "Received",
+            "Pending refund",
+            "All",
+          ].map((status, index) => (
+            <div key={index} className="relative">
               <button
-                key={index}
-                className={`py-2 px-4 text-sm transition duration-300 ${selectedStatus === status
-                    ? 'border-b-2 border-[#0CBB7D] font-semibold text-black'
-                    : 'text-gray-500'
-                  }`}
+                className={`py-2 text-sm transition duration-300 ${selectedStatus === status ? "text-black font-medium" : "text-gray-500 hover:text-black"}`}
                 onClick={() => setSelectedStatus(status)}
               >
                 {status}
               </button>
-            ))}
-          </div>
-
-          <div className="bg-white shadow-md rounded-lg mb-4">
-            <div className="border-b p-4 flex font-bold text-left">
-              <input type="checkbox" onChange={handleSelectAll} />
-              {getHeaders(selectedStatus).map((header, index) => (
-                <div key={index} className="flex-1">{header}</div>
-              ))}
+              {selectedStatus === status && (
+                <div className="absolute bottom-[-2px] left-0 right-0 mx-auto h-[2px] w-[100%] bg-[#0CBB7D] rounded-full transition-all duration-300"></div>
+              )}
             </div>
-
-            {filteredOrders.length > 0 ? (
-              filteredOrders.map(order => (
-                <div key={order.id} className="border-b p-4 flex items-center text-left space-x-4">
-                  <input
-                    type="checkbox"
-                    checked={selectedOrders[order.id] || false}
-                    onChange={() => handleSelectOrder(order.id)}
-                    className="flex-shrink-0"
-                  />
-                  <div className="flex-1">
-                    <p className="font-semibold">
-                      {/* Format Request Id & Date */}
-                      <span>{order.id}</span> <br />
-                      <span>{new Date(order.date).toLocaleDateString('en-GB', { year: 'numeric', month: 'short', day: 'numeric' })} |</span>
-                      <span> {new Date(order.date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).replace(':', '.') } </span> <br />
-                      <span className="text-gray-600">CUSTOM</span> <br />
-                      <a href="#" className="text-[#0CBB7D] ml-0 mt-1">View product</a>
-                    </p>
-                  </div>
-
-                  <div className="flex-1">
-                    <p className="font-semibold">{order.buyerName}</p>
-                    <p>{order.email}</p>
-                  </div>
-                  <div className="flex-1">
-                    <p className="font-semibold">{order.product}</p>
-                    <p className="px-3 py-1 bg-[#F4F4F4] text-[#1B8D46] rounded-full text-sm shadow-md inline-block mt-1">
-                      Prepaid
-                    </p>
-                  </div>
-
-                  {order.shippingAddress ? (
-                    <div className="flex-1">
-                      <p>{order.shippingAddress}</p>
-                    </div>
-                  ) : (
-                    <div className="flex-1">
-                      <p>{order.buyerDetails}</p>
-                    </div>
-                  )}
-
-                  <div className="flex-1">
-                    <p>{order.refundDetails}</p>
-                  </div>
-
-                  <div className="flex-1">
-                    <span className="px-3 py-1 border-none bg-[#DFF5E0] rounded-full text-sm text-[#1B8D46] shadow-md">
-                      {order.status}
-                    </span>
-                  </div>
-
-                  <div className="flex-1">
-                    <button onClick={() => handleShipOrder(order.id)} className="px-2 py-1 bg-[#0CBB7D] text-white rounded-md hover:bg-[#0AA56F]">
-                      Schedule Pick up
-                    </button>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <p className="p-4 text-center">No orders found.</p>
-            )}
-          </div>
+          ))}
         </div>
+      </div>
+
+      {/* Header Container */}
+      <div className="bg-white shadow-md rounded-lg mb-4">
+        <div className="border-b p-4 flex font-medium text-left bg-[#F4F4F4]">
+          <input type="checkbox" onChange={handleSelectAll} />
+          {getHeaders(selectedStatus).map((header, index) => (
+            <div key={index} className="flex-1 text-[#1A1A1A]">{header}</div>
+          ))}
+        </div>
+      </div>
+
+      {/* Order Details Container with gap */}
+      <div className="space-y-4">
+        {filteredOrders.length > 0 ? (
+          filteredOrders.map(order => (
+            <div key={order.id} className="bg-[#F4F4F4] shadow-md rounded-lg p-4 mb-4">
+              <div className="flex items-center space-x-4">
+                <input
+                  type="checkbox"
+                  checked={selectedOrders[order.id] || false}
+                  onChange={() => handleSelectOrder(order.id)}
+                  className="w-4 h-4 border-2 border-[#0CBB7D] bg-white rounded-md cursor-pointer peer checked:bg-[#0CBB7D] checked:border-[#0CBB7D] focus:outline-none"
+                />
+                <div className="flex-1">
+                  <p className="font-semibold text-[#1A1A1A] text-[14px]">
+                    <span>{order.id}</span> <br />
+                    <span>{new Date(order.date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })} | {new Date(order.date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).replace(':', '.')}</span> <br />
+                    <span className="text-gray-600">CUSTOM</span> <br />
+                    <a href="#" className="text-[#0CBB7D] ml-0 mt-1 border-b-2 border-[#0CBB7D]">View product</a>
+                  </p>
+                </div>
+
+                <div className="flex-1">
+                  <p className="font-semibold text-[#1A1A1A]">{order.buyerName}</p>
+                  <p>{order.email}</p>
+                </div>
+
+                <div className="flex-1">
+                  <p className="font-semibold text-[#1A1A1A]">{order.product}</p>
+                  <p className="px-3 py-1 bg-[#F4F4F4] text-[#1B8D46] rounded text-sm shadow-md inline-block mt-1">Prepaid</p>
+                </div>
+
+                <div className="flex-1">
+                  <p>{order.shippingAddress}</p>
+                </div>
+
+                <div className="flex-1">
+                  <p>{order.refundDetails}</p>
+                </div>
+
+                <div className="flex-1">
+                  <span className="px-3 py-1 border-none bg-[#DFF5E0] rounded-full text-sm text-[#1B8D46] shadow-md">
+                    {order.status}
+                  </span>
+                </div>
+
+                <div className="flex-1">
+                  <button onClick={() => handleShipOrder(order.id)} className="px-2 py-1 bg-[#0CBB7D] text-white rounded-md hover:bg-[#0AA56F]">
+                    Schedule Pick up
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="p-4 text-gray-600">No orders available</p>
+        )}
       </div>
     </div>
   );
