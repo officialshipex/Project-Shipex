e2342b93b239d2ef8b4146a7deebb75e4100e235
Your Name <your-email@example.com>
2024-12-02 13:29:44 +0530

    update done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import 'typeface-poppins';
 // import { useState, useEffect } from "react";
 // import NavBar from "./components/Common/NavBar";
 // import Dashboard from "./components/Dashboard/Dashboard"; // Import Dashboard
-// import OrderDashboard from "./components/Dashboard/OrderDashboard"; // Import OrderDashboard
-// import OrderList from "./components/Dashboard/Order"; // Import OrderList
+import OrderDashboard from "./components/Dashboard/OrderDashboard"; // Import OrderDashboard
+ //import OrderList from "./components/Dashboard/Order"; // Import OrderList
 //import ReturnList from "./components/Dashboard/Main_Return_Request"; // Import only ReturnList
- import BillingList from "./components/Dashboard/Main_Billing"; // Import BillingList
+ //import BillingList from "./components/Dashboard/Main_Billing"; // Import BillingList
 // import NDR from "./components/Dashboard/Main_ndr"; // Commented out NDR import
 
 // import CRFIDPopup from "./components/Dashboard/Billing_COD Remittance_CRF id";
@@ -18,11 +18,14 @@ import 'typeface-poppins';
  // import AddOrder from "./components/AddOrder/Add Order_Single Shipment";
 // import CourierPage from "./components/AddOrder/courierpage";
  // import QuickShipment from "./components/AddOrder/Quickshipment"
-// import BulkOrders from "./components/AddOrder/BulkOrders";
+// import BulkOrders from "./components/AddOrder/BulkOrders";\
+//import DeliveryPerformance from './components/Dashboard 1/DeliveryPerformance';
+//import ToolsActivity from './components/Dashboard 1/Activity-log';
+
 function App() {
   return (
     <>
-      <BillingList />
+  <OrderDashboard></OrderDashboard>
     </>
   );
 }
