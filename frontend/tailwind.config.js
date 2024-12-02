37c2af36e811ca84a5ad1e7958b91801a47f7ae6
Dactar <kulmc01@gmail.com>
2024-12-02 14:42:07 +0530

    solve conflict

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0CBB7D',
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-down': 'slideDown 0.8s ease-out',
+      },
+    },
   },
   plugins: [],
-}
-
+};
