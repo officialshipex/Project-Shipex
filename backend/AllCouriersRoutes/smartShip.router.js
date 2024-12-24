4e5757975febb2061b9cf11b003c27deda748e0d
Kh-ushi <khushigupta31052002@gmail.com>
2024-12-24 20:33:51 +0530

    add/enabel/disable SmartShip and Services

---
+++ backend/AllCouriersRoutes/smartShip.router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const {
+    OneStapOrderRegisteration,
+    orderRegistration,
+    getOrderDetails,
+    createManifest,
+    getShippingLabel,
+    cancelOrder,
+    trackOrderByTrackingID,
+    trackOrderByRequestorderId,
+    trackOrderByOrderReferenceId,
+    getCouriers} = require('../AllCouriers/SmartShip/Couriers/couriers.controller');
+
+const  {saveSmartShip,isEnabeled}= require('../AllCouriers/SmartShip/Authorize/smartShip.controller');
+
+const router = express.Router();
+
+// router.post('/get-token',getAccessToken)
+router.get("/saveNew",saveSmartShip);
+router.get("/isEnabeled",isEnabeled);
+router.get("/getCouriers",getCouriers);
+
+router.post('/one-step-order-register',OneStapOrderRegisteration);
+router.post('/order-register',orderRegistration);
+router.post('/get-order-details',getOrderDetails);
+router.post('/create-manifest',createManifest); 
+router.post('/get-shipping-label',getShippingLabel);
+router.post('/cancel-order',cancelOrder);
+router.get('/track-by-number',trackOrderByTrackingID);
+router.get('/track-by-request-id',trackOrderByRequestorderId);
+router.get('/track-by-referance-id',trackOrderByOrderReferenceId);
+
+// router.post('/hub-register',hubRegistration);
+// router.post('/get-hub-details',getHubDetails);
+// router.post('/delete-Hub',deleteHub);
+
+module.exports = router;
