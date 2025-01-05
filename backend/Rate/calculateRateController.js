7c0cb280deaaf9acfa3a9e2cc55687fc565964e0
Kh-ushi <khushigupta31052002@gmail.com>
2025-01-05 14:52:17 +0530

    ship order in process

--- backend/Rate/calculateRateController.js
+++ backend/Rate/calculateRateController.js
@@ -2,6 +2,8 @@ const RateCard = require("../models/rateCards.js");
 const zoneManagementController = require("./zoneManagementController.js");
 const getZone = zoneManagementController.getZone;
 
+
+
 const calculateRate = async (req, res) => {
   try {
     let result = await getZone(req.body.pickupPincode, req.body.deliveryPincode);
@@ -83,4 +85,84 @@ const calculateRate = async (req, res) => {
   }
 };
 
-module.exports = { calculateRate };
+
+async function calculateRateForService(payload) {
+  try {
+    const{pickupPincode, deliveryPincode,length,breadth,height,weight, cod, valueInINR,filteredServices,rateCardType}=payload;
+    const result = await getZone(pickupPincode.toString(), deliveryPincode.toString());
+   
+    const currentZone = result.zone;
+    const ans = [];
+    const l = parseFloat(length);
+    const b = parseFloat(breadth);
+    const h = parseFloat(height);
+    const deadweight = parseFloat(weight);
+    const volumetricWeight = (l * b * h) / 5000;
+    const chargedWeight = Math.max(deadweight, volumetricWeight);
+    let codCharge = 0;
+    const gstRate = 18;
+
+    const rateCards=[];
+    for(fls of filteredServices){
+
+      let currentRate=await RateCard.findOne({type:rateCardType,courierProviderName:fls.courierProviderName,courierServiceName:fls.courierProviderServiceName});
+      rateCards.push(currentRate);
+    }
+
+    const finalRate=rateCards.filter(Boolean);
+
+    for (const rc of finalRate) {
+      const basicWeight = parseFloat(rc.weightPriceBasic[0].weight);
+      const additionalWeight = parseFloat(rc.weightPriceAdditional[0].weight);
+      const basicChargeForward = parseFloat(rc.weightPriceBasic[0][currentZone].forward);
+      const additionalChargeForward = parseFloat(rc.weightPriceAdditional[0][currentZone].forward);
+      const finalBasicChargeForward = basicChargeForward;
+      const finalAdditionalChargeForward = Math.max(0, Math.ceil((chargedWeight - basicWeight) / additionalWeight) * additionalChargeForward);
+      const totalForwardCharge = finalBasicChargeForward + finalAdditionalChargeForward;
+      const basicChargeRTO = parseFloat(rc.weightPriceBasic[0][currentZone].rto);
+      const additionalChargeRTO = parseFloat(rc.weightPriceAdditional[0][currentZone].rto);
+      const finalBasicChargeRTO = basicChargeRTO;
+      const finalAdditionalChargeRTO = Math.ceil((chargedWeight - basicWeight) / additionalWeight) * additionalChargeRTO;
+      const totalRTOCharge = finalBasicChargeRTO + finalAdditionalChargeRTO;
+
+      if (cod === 'Yes') {
+        const orderValue = Number(valueInINR) || 0;
+        if (typeof rc.codCharge === 'number' && typeof rc.codPercent === 'number') {
+          const calculatedCodCharge = Math.max(rc.codCharge, orderValue * (rc.codPercent / 100));
+          codCharge += calculatedCodCharge;
+        } else {
+          console.error("COD charge or percentage is not properly defined.");
+        }
+      }
+
+      const gstAmountForward = ((totalForwardCharge + codCharge) * (gstRate / 100)).toFixed(2);
+      const totalChargesForward = ((totalForwardCharge + codCharge) + (totalForwardCharge + codCharge) * (gstRate / 100)).toFixed(2);
+      const gstAmountRTO = ((totalRTOCharge + codCharge) * (gstRate / 100)).toFixed(2);
+      const totalChargesRTO = ((totalRTOCharge + codCharge) + (totalRTOCharge + codCharge) * (gstRate / 100)).toFixed(2);
+
+      const allRates = {
+        courierServiceName: rc.courierServiceName,
+        cod: codCharge,
+        forward: {
+          charges: totalForwardCharge,
+          gst: gstAmountForward,
+          finalCharges: totalChargesForward
+        },
+        rto: {
+          charges: totalRTOCharge,
+          gst: gstAmountRTO,
+          finalCharges: totalChargesRTO
+        }
+      };
+
+      ans.push(allRates);
+    }
+    return ans;
+  } catch (error) {
+    console.error('Error in Calculation:', error);
+    throw new Error('Error in Calculation');
+  }
+}
+
+
+module.exports = { calculateRate,calculateRateForService};
