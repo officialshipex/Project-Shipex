16564b8e368c407c7f14306b1aece4ef631e1596
Kh-ushi <khushigupta31052002@gmail.com>
2025-01-20 22:46:18 +0530

    Live Setup Done

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,40 @@
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config();
+}
 const mongoose = require('mongoose');
-require('dotenv').config(); 
+const session=require('express-session');
+const MongoStore=require('connect-mongo');
 
-async function connection (){
-    try{
+const store=MongoStore.create({
+    mongoUrl:process.env.MONGODB_URI,
+    crypto:{
+        secret:process.env.MONGO_SECRET,
+    },
+    touchAfter:24*3600
+});
+
+store.on("error",()=>{
+console.log("ERROR IN MONGO STORE",err);
+})
+
+const sessionOptions={
+    store,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24*7 
+    }
+}
+
+app.use(session(sessionOptions));
+
+async function connection() {
+    try {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('Database connected');
-    }catch(err){
+    } catch (err) {
         console.log(err);
         throw err;
     }
