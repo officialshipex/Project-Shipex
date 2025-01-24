a92a7d62a0c5507eadc16bc86a3553cbbaafb57d
JyotiprakashOP <jyotiprakash24263@gmail.com>
2025-01-24 18:21:27 +0530

    merging updated

--- backend/label/printInvoice.controller.js
+++ backend/label/printInvoice.controller.js
@@ -1,68 +1,12 @@
 const express = require("express");
-<<<<<<< HEAD
-const Invoice=require("./printInvoice.model")
-const PDFDocument = require("pdfkit");
-const fs = require("fs");
-const path = require("path");
-const{generateStyledInvoiceWithPages}=require("./InvoiceStyle");
-=======
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 const path = require("path");
 // const { generateStyledInvoiceWithPages } = require("./InvoiceStyle");
->>>>>>> 2f5fb255d19459b4d181857acf2a4bda5760fa09
 
 const router = express.Router();
 router.use(express.json());
 
-<<<<<<< HEAD
-const invoiceDemo={
-    "_id": "63bcd2f8e92f4700192f8a1d",
-    "invoiceNumber": "1736312705",
-    "invoiceDate": "Jan 08, 2025",
-    "customerName": "Ravi Yadav",
-    "billingAddress": "Main Road, Bus Stand, Vpo Chhillar, Rewari, Haryana, 123401",
-    "shippingAddress": "Main Road, Bus Stand, Vpo Chhillar, Rewari, Haryana, 123401",
-    "paymentMethod": "COD",
-    "AWB": "SF1544098244NIM",
-    "product": {
-      "name": "Shoes",
-      "sku": "SH12345",
-      "hsn": "640391",
-      "quantity": 1,
-      "unitPrice": 1100
-    },
-    "totalAmount": 1100
-  }
-  
-
-  router.get("/generate-invoice/:id", async (req, res) => {
-    try {
-      const selectedItems = req.headers['custom-data']
-        ? JSON.parse(req.headers['custom-data'])
-        : null;
-  
-      if (selectedItems==null) {
-        return res.status(404).json({ message: "Invoice not found" });
-      }
-  
-      res.setHeader("Content-Type", "application/pdf");
-      res.setHeader(
-        "Content-Disposition",
-        `attachment; filename="shipping_invoice.pdf"`
-      );
-  
-      generateStyledInvoiceWithPages(selectedItems,res);
-    } catch (error) {
-      console.log(error);
-      console.error("Error generating the invoice:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  });
-  
-
-  module.exports=router
-=======
 // const invoiceDemo = {
 //   _id: "63bcd2f8e92f4700192f8a1d",
 //   invoiceNumber: "1736312705",
@@ -426,4 +370,3 @@ router.get("/generate-invoice", async (req, res) => {
 });
 
 module.exports = router;
->>>>>>> 2f5fb255d19459b4d181857acf2a4bda5760fa09
--- backend/label/printLabel.controller.js
+++ backend/label/printLabel.controller.js
@@ -1,35 +1,5 @@
 const express = require("express");
 const PDFDocument = require("pdfkit");
-<<<<<<< HEAD
-// const Order = require("./printLabel.model");
-
-const Order={
-    "customerName": "Sachin Kumar",
-    "address": "Main Bus Stand, Vpo Ismaila, 11B, Sampla, ROHTAK, Haryana, India - 124501",
-    "mobile": "8607000463",
-    "orderDate": "Dec 19, 2024",
-    "invoiceNo": "1734587437",
-    "amount": 700,
-    "weight": "0.5 KG",
-    "dimensions": "12 X 11 X 12",
-    "sku": "Clothes",
-    "itemName": "Clothes",
-    "quantity": 1,
-    "pickupAddress": "Rajat Kumar Funky House, Dadri Gate, Kount Road Bhiwani, Haryana, India - 127021",
-    "pickupMobile": "7988477367"
-  }
-  
-
-const router = express.Router();
-
-// Route to generate PDF
-router.get("/generate-pdf", async (req, res) => {
-  try {
-    // Fetch the first order (demo purpose)
-    // const orderData = await Order.findOne();
-    // console.log(orderData)
-    const orderData=Order;
-=======
 const LabelSetting = require("./label.model");
 const bwipjs = require("bwip-js");
 
@@ -66,15 +36,10 @@ router.get("/generate-pdf", async (req, res) => {
   try {
     // console.log("I am being called");
     const orderData = Order;
->>>>>>> 2f5fb255d19459b4d181857acf2a4bda5760fa09
     if (!orderData) {
       return res.status(404).send("Order not found");
     }
 
-<<<<<<< HEAD
-    // Create a PDF document
-    const doc = new PDFDocument();
-=======
     const user_id = "67237ee56a42044404423c8e";
     const settings = await LabelSetting.findOne({ user: user_id });
 
@@ -92,44 +57,12 @@ router.get("/generate-pdf", async (req, res) => {
     });
 
     const doc = new PDFDocument({ size: "A4", margin: 50 });
->>>>>>> 2f5fb255d19459b4d181857acf2a4bda5760fa09
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader(
       "Content-Disposition",
       `attachment; filename="shipping_label.pdf"`
     );
 
-<<<<<<< HEAD
-    // Generate PDF content
-    doc.fontSize(14).text(`To: ${orderData.customerName}`, { align: "left" });
-    doc.text(`Address: ${orderData.address}`);
-    doc.text(`Mobile: ${orderData.mobile}`);
-    doc.moveDown();
-
-    doc.text(`Order Date: ${orderData.orderDate}`);
-    doc.text(`Invoice No: ${orderData.invoiceNo}`);
-    doc.text(`COD: ₹${orderData.amount}`);
-    doc.text(`Weight: ${orderData.weight}`);
-    doc.text(`Dimensions (cm): ${orderData.dimensions}`);
-    doc.moveDown();
-
-    doc.text(`SKU: ${orderData.sku}`);
-    doc.text(`Item Name: ${orderData.itemName}`);
-    doc.text(`Quantity: ${orderData.quantity}`);
-    doc.text(`Total Amount: ₹${orderData.amount}`);
-    doc.moveDown();
-
-    doc.text("Pickup and Return Address:");
-    doc.text(orderData.pickupAddress);
-    doc.text(`Mobile No: ${orderData.pickupMobile}`);
-    doc.moveDown();
-
-    doc.text(
-      "Note: This is a computer-generated document and does not require a signature."
-    );
-
-    // Pipe the PDF to the response
-=======
     // Header Section
     doc.fontSize(16).font("Helvetica-Bold").text(`To:`, { align: "left" });
     doc
@@ -356,7 +289,6 @@ router.get("/generate-pdf", async (req, res) => {
       });
 
     // Pipe PDF to response
->>>>>>> 2f5fb255d19459b4d181857acf2a4bda5760fa09
     doc.pipe(res);
     doc.end();
   } catch (error) {
