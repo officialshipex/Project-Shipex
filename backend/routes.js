2a627221827a45d9ac5fb0b736c644d09591dda8
lulu <daslaxmikanta219@gmail.com>
2025-02-13 11:40:24 +0530

    update

--- backend/routes.js
+++ backend/routes.js
@@ -53,7 +53,7 @@ router.use('/courierServices',CourierServiceRoutes)
 
 // router.use('/order', orderRouter);
 router.use("/order", isAuthorized, newOrderRoute);
-router.use("/order", orderRouter);
+// router.use("/order", orderRouter);
 //create product route
 // router.use("/products",productRouter)
 
