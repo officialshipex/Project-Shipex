dcb9c385e25d91b132803f767bb81ae431b9adcf
JyotiprakashOP <jyotiprakash24263@gmail.com>
2025-02-22 02:02:54 +0530

    updated with rate and assign

--- backend/AllCouriers/getActiveCourier.js
+++ backend/AllCouriers/getActiveCourier.js
@@ -9,4 +9,20 @@ const getAllCouriers = async (req, res) => {
     }
   };
 
-  module.exports = getAllCouriers;
+
+  const deleteCourier = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedCourier = await AllCourier.findByIdAndDelete(id);
+  
+      if (!deletedCourier) {
+        return res.status(404).json({ message: 'Courier not found' });
+      }
+  
+      res.status(200).json({ message: 'Courier deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  module.exports = {getAllCouriers, deleteCourier};
