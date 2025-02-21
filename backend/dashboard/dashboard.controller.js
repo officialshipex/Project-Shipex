1c2dc3f0c98b3e38222f5ce94adc1d924f26c987
lulu <daslaxmikanta219@gmail.com>
2025-02-21 15:43:52 +0530

    merge

--- backend/dashboard/dashboard.controller.js
+++ backend/dashboard/dashboard.controller.js
@@ -18,14 +18,34 @@ const dashboard = async (req, res) => {
         return total + order.paymentDetails.amount; // Sum up the amount for each delivered order
       }, 0);
       //Average Shipping
-      
+     const averageShipping=totalRevenue/Delivered;
+
+     const pending = orders.filter(
+        (order) => order.status === "new"
+      );
+     const pendingOrder=pending.length
      //total order
+     const Intransit=orders.filter(
+        (order) => order.status === "In Transit"
+      );
+     const intransite=Intransit.length
       const totalorder = orders.length;
   
+
+
+   
+
+// console.log(xpreesbees)
+      
       return res.status(200).json({
+       
         totalorder,
         totalRevenue,
-        Delivered
+        Delivered,
+        averageShipping,
+        pendingOrder,
+        intransite,
+    
       });
     } catch (error) {
       return res.status(400).json({ message: "Order not present" });
