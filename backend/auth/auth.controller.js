fea0bb70b1661ff9f87dd5b807ab6d25f3021f45
bijaybhanja <bhanjabijayketan@gmail.com>
2025-03-10 16:58:08 +0530

    update register

--- backend/auth/auth.controller.js
+++ backend/auth/auth.controller.js
@@ -1,27 +1,57 @@
-if(process.env.NODE_ENV!="production"){
-  require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 const { validateForm, validateEmail } = require("../utils/afv");
 const User = require("../models/User.model");
+const RateCard=require("../models/rateCards")
+const Plan=require("../models/Plan.model")
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const {sendWelcomeEmail}=require("../notification/welcomeNotification")
-const FRONTEND_URL =process.env.NODE_ENV!="production"?"http://localhost:5173":process.env.FRONTEND_URL;
+const { sendWelcomeEmail } = require("../notification/welcomeNotification");
+const FRONTEND_URL =
+  process.env.NODE_ENV != "production"
+    ? "http://localhost:5173"
+    : process.env.FRONTEND_URL;
 //for User Registration
 const register = async (req, res) => {
   try {
-    const {fullname, email, phoneNumber, company, monthlyOrders, password, confirmedPassword, checked } = req.body;
+    const {
+      fullname,
+      email,
+      phoneNumber,
+      company,
+      monthlyOrders,
+      password,
+      confirmedPassword,
+      checked,
+    } = req.body;
     console.log(req.body);
-    
 
-    if (!fullname || !email || !phoneNumber || !company || !monthlyOrders || !password || !confirmedPassword) {
+    if (
+      !fullname ||
+      !email ||
+      !phoneNumber ||
+      !company ||
+      !monthlyOrders ||
+      !password ||
+      !confirmedPassword
+    ) {
       return res.status(400).json({
         success: false,
         message: "Please fill all the fields",
       });
     }
 
-    const userData = { fullname, email, phoneNumber, company, monthlyOrders, password, confirmedPassword, checked };
+    const userData = {
+      fullname,
+      email,
+      phoneNumber,
+      company,
+      monthlyOrders,
+      password,
+      confirmedPassword,
+      checked,
+    };
 
     const validateFields = validateForm(userData);
 
@@ -33,11 +63,11 @@ const register = async (req, res) => {
     }
     let userId;
     let isUnique = false;
-  
+
     while (!isUnique) {
       userId = Math.floor(10000 + Math.random() * 90000); // Generates a random five-digit number
       const existingUser = await User.findOne({ userId });
-  
+
       if (!existingUser) {
         isUnique = true;
       }
@@ -46,13 +76,6 @@ const register = async (req, res) => {
     const userPhoneNumber = await User.findOne({ phoneNumber });
     const userCompany = await User.findOne({ company });
 
-    if (userEmail) {
-      return res.status(400).json({
-        success: false,
-        message: "Email or User already exists",
-      });
-    }
-
     if (userPhoneNumber) {
       return res.status(400).json({
         success: false,
@@ -60,6 +83,12 @@ const register = async (req, res) => {
       });
     }
 
+    if (userEmail) {
+      return res.status(400).json({
+        success: false,
+        message: "Email or User already exists",
+      });
+    }
     if (userCompany) {
       return res.status(400).json({
         success: false,
@@ -67,23 +96,41 @@ const register = async (req, res) => {
       });
     }
 
-
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
-     fullname,
+      fullname,
       email,
       phoneNumber,
       company,
       monthlyOrders,
       password: hashedPassword,
-      userId
-
+      userId,
     });
 
     await newUser.save();
-    await sendWelcomeEmail(email,fullname)
-    
+    await sendWelcomeEmail(email, fullname);
+
+    // Fetch the "Bronze" rate card
+    const bronzeRateCard = await RateCard.find({ plan: "bronze" });
+
+    if (!bronzeRateCard) {
+      return res.status(500).json({
+        success: false,
+        message: "Bronze rate card not found",
+      });
+    }
+
+    // Assign the "Bronze" rate card to the new user
+    const newPlan = new Plan({
+      userId: newUser._id,
+      userName: fullname,
+      planName: "bronze", 
+      rateCard: bronzeRateCard, // Assigning the fetched rate card
+    });
+
+    await newPlan.save();
+
     const payload = {
       user: {
         id: newUser._id,
@@ -92,37 +139,35 @@ const register = async (req, res) => {
       },
     };
 
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    //welcome email
 
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
-     //welcome email
-    
     return res.status(200).json({
       success: true,
       message: "User registered successfully",
       data: token,
-    })
-
+    });
   } catch (error) {
     console.log("error", error);
     return res.status(500).json({
       success: false,
       message: "Internal server error",
-    })
+    });
   }
 };
 
-
-//For User Login 
+//For User Login
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    
 
     if (!email || !password) {
       return res.status(400).json({
         success: false,
         message: "Please fill all the fields",
-      })
+      });
     }
 
     const validateFields = validateEmail(email);
@@ -131,7 +176,7 @@ const login = async (req, res) => {
       return res.status(400).json({
         success: false,
         message: "Invalid email ",
-      })
+      });
     }
 
     const user = await User.findOne({ email });
@@ -140,7 +185,7 @@ const login = async (req, res) => {
       return res.status(400).json({
         success: false,
         message: "User does not exist",
-      })
+      });
     }
 
     const checkPassword = await bcrypt.compare(password, user.password);
@@ -149,7 +194,7 @@ const login = async (req, res) => {
       return res.status(400).json({
         success: false,
         message: "Password is incorrect",
-      })
+      });
     }
 
     const payload = {
@@ -160,10 +205,11 @@ const login = async (req, res) => {
         kyc: user.kycDone,
         isAdmin: user.isAdmin,
       },
-    }
+    };
 
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
-
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
     return res.status(200).json({
       success: true,
@@ -171,18 +217,16 @@ const login = async (req, res) => {
       kyc: user.kycDone,
       data: token,
     });
-
   } catch (error) {
     console.log("error", error);
     return res.status(500).json({
       success: false,
       message: "Internal server error",
-    })
+    });
   }
 };
 
-
-//For successfull Google login  
+//For successfull Google login
 const googleLogin = async (req, res) => {
   try {
     const profile = req.user;
@@ -195,7 +239,7 @@ const googleLogin = async (req, res) => {
         monthlyOrders: profile.monthlyOrders || 0,
         googleOAuthID: profile.id,
         isVerified: profile.email_verified,
-        provider: 'Google',
+        provider: "Google",
       });
 
       await newUser.save();
@@ -208,9 +252,11 @@ const googleLogin = async (req, res) => {
         email: user.email,
         fullname: user.fullname,
       },
-    }
+    };
 
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
     return res.redirect(`${FRONTEND_URL}/login?token=${token}`);
 
@@ -219,9 +265,6 @@ const googleLogin = async (req, res) => {
     //   message: "User logged in successfully",
     //   data: token,
     // })
-
-
-
   } catch (error) {
     console.log("error", error);
     return res.redirect(`${FRONTEND_URL}`);
@@ -229,10 +272,10 @@ const googleLogin = async (req, res) => {
     //   success: false,
     //   message: "Internal server error",
     // });
-  };
-}
+  }
+};
 
-//for failure google login 
+//for failure google login
 const googleLoginFail = (req, res) => {
   try {
     return res.status(400).json({
@@ -299,7 +342,6 @@ const verifySession = async (req, res) => {
       kyc: user.kycDone,
       message: "Token verified",
     });
-
   } catch (error) {
     console.log("error", error);
     return res.status(500).json({
@@ -309,11 +351,10 @@ const verifySession = async (req, res) => {
   }
 };
 
-
 module.exports = {
   register,
   login,
   googleLogin,
   googleLoginFail,
-  verifySession
+  verifySession,
 };
