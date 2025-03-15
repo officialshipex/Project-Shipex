667219c688407561dd4648f03b2e8a4a4d6a3424
lulu <daslaxmikanta219@gmail.com>
2025-03-16 00:50:48 +0530

    merge

---
+++ backend/COD/CodRemittanceOrder.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const CodRemittanceOrderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  totalCodRemittance: {
+    type: Number,
+  },
+  totalCodRemittancePaid: {
+    type: Number,
+  },
+  totalCodRemittanceDue: {
+    type: Number,
+  },
+  codRemittanceOrderData: [
+    {
+      Date: {
+        type: Date,
+      },
+      orderID: {
+        type: String,
+      },
+      userName: {
+        type: String,
+      },
+      PhoneNumber: {
+        type: String,
+      },
+      Email: {
+        type: String,
+      },
+      AWB_Number: {
+        type: String,
+      },
+      CODAmount: {
+        type: String,
+      },
+      status: {
+        type: String,
+        enum: ["Pending", "Paid"],
+        default: "Pending",
+      },
+    },
+  ],
+});
+
+const CodRemittanceOrder = mongoose.model(
+  "CodRemittanceOrder",
+  CodRemittanceOrderSchema
+);
+module.exports = CodRemittanceOrder;
