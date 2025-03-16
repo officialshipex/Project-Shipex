b1a0e8bf8413ea5322b7098b304838311aac276a
bijaybhanja <bhanjabijayketan@gmail.com>
2025-03-16 12:33:15 +0530

    done

--- backend/Channels/allChannel.routes.js
+++ backend/Channels/allChannel.routes.js
@@ -5,10 +5,11 @@ const app=express();
 app.use(express.json());
 
 const{storeAllChannelDetails,webhookhandler,getOrders,getAllChannel,getOneChannel,updateChannel,deleteChannel,fulfillOrder}=require("./allChannel.controller")
-
+const {wooCommerceWebhookHandler}=require("./WooCommerce/woocommerce.controller")
 router.post("/storeAllChannelDetails",isAuthorized,storeAllChannelDetails)
 // ✅ Apply `express.raw()` Middleware Only for Webhook
 router.post("/webhook/orders", express.raw({ type: "application/json" }), webhookhandler);
+router.post("/webhook/woocommerce",express.raw({type:"application/json"}),wooCommerceWebhookHandler)
 router.get("/getAllChannel",isAuthorized,getAllChannel)
 router.get("/getOneChannel/:id",isAuthorized,getOneChannel)
 router.put("/updateChannel/:id",isAuthorized,updateChannel)
