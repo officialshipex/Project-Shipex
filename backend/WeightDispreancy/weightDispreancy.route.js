964300c57ed2c4303f0407295a09998d8324b4b5
bijaybhanja <bhanjabijayketan@gmail.com>
2025-03-26 00:02:33 +0530

    done

--- backend/WeightDispreancy/weightDispreancy.route.js
+++ backend/WeightDispreancy/weightDispreancy.route.js
@@ -2,8 +2,9 @@ const express=require("express");
 const router=express.Router();
 const multer=require("multer");
 const upload=multer({dest:'uploads/'});
+const { uploads } = require("../config/s3");
 
-const {downloadExcel,uploadDispreancy,AllDiscrepancy,AllDiscrepancyBasedId,AcceptDiscrepancy,AcceptAllDiscrepancies}=require("./weightDispreancy.controller");
+const {downloadExcel,uploadDispreancy,AllDiscrepancy,AllDiscrepancyBasedId,AcceptDiscrepancy,AcceptAllDiscrepancies,raiseDiscrepancies,adminAcceptDiscrepancy,declineDiscrepancy}=require("./weightDispreancy.controller");
 
 router.post("/upload",upload.single('file'),uploadDispreancy);
 router.get("/download-excel",downloadExcel)
@@ -11,5 +12,9 @@ router.get("/allDispreancy",AllDiscrepancy)
 router.get("/allDispreancyById",AllDiscrepancyBasedId)
 router.post("/acceptDiscrepancy",AcceptDiscrepancy)
 router.post("/acceptAllDiscrepancies",AcceptAllDiscrepancies)
+router.post("/raiseDiscrepancies", uploads.single("image"), raiseDiscrepancies);
+router.post("/adminAcceptDiscrepancy",adminAcceptDiscrepancy)
+router.post("/declineDiscrepancy",declineDiscrepancy)
+// router.get("/all", getAllPosts);
 
 module.exports=router;
