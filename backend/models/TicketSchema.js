ac4702792c2c1867b1fc76ee0e228beb8e3690a7
JyotiprakashOP <jyotiprakash24263@gmail.com>
2025-03-26 21:17:08 +0530

    this is support

--- backend/models/TicketSchema.js
+++ backend/models/TicketSchema.js
@@ -4,7 +4,10 @@ const ticketSchema = new mongoose.Schema({
   category: { type: String, required: true },
   subcategory: { type: String, required: true },
   awbType: { type: String, enum: ["single", "multiple"], required: true },
-  awbNumbers: [{ type: String, required: function () { return this.awbType === "multiple"; } }],
+  awbNumbers: [{ 
+    type: String, 
+    required: function () { return this.awbType === "multiple"; } 
+  }],
   ticketNumber: { type: String, unique: true, required: true },
   file: { type: String }, // Store file path if uploaded
   createdAt: { type: Date, default: Date.now },
@@ -15,7 +18,14 @@ const ticketSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   email: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
-  company: { type: String, required: true }
+  company: { type: String, required: true },
+
+  // Status field
+  status: { 
+    type: String, 
+    enum: ["active", "resolved", "deleted"], 
+    default: "active" 
+  }
 });
 
 const Ticket = mongoose.model("Ticket", ticketSchema);
