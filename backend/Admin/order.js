44fb68ed64ddc278792cca9c8c1e207c1f24a6c7
bijaybhanja <bhanjabijayketan@gmail.com>
2025-04-15 10:40:23 +0530

    done

--- backend/Admin/order.js
+++ backend/Admin/order.js
@@ -1,4 +1,5 @@
 const Order=require("../models/newOrder.model")
+const User=require("../models/User.model")
 
 const getOrdersByStatus = async (req, res) => {
     try {
@@ -16,10 +17,15 @@ const getOrdersByStatus = async (req, res) => {
   
       const totalCount = await Order.countDocuments(filter);
   
-      let query = Order.find(filter).sort({ createdAt: -1 });
+      let query = Order.find(filter)
+        .sort({ createdAt: -1 })
+        .populate("userId", "fullname email phoneNumber company"); // Select only required fields
+  
       if (limit) query = query.skip(skip).limit(limit);
   
       const orders = await query.lean();
+
+    //   console.log("ordder",orders[0])
       const totalPages = limit ? Math.ceil(totalCount / limit) : 1;
   
       res.json({
@@ -33,5 +39,6 @@ const getOrdersByStatus = async (req, res) => {
       res.status(500).json({ error: "Internal server error" });
     }
   };
+  
 
   module.exports={getOrdersByStatus}
