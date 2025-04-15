87b784b2b61291c2e9a26f13677c5cdf9cbb7a86
bijaybhanja <bhanjabijayketan@gmail.com>
2025-04-15 10:41:06 +0530

    done

--- backend/Orders/newOrder.controller.js
+++ backend/Orders/newOrder.controller.js
@@ -1038,7 +1038,7 @@ const startTrackingLoop = async () => {
   }
 };
 
-startTrackingLoop()
+// startTrackingLoop()
 
 const mapTrackingResponse = (data, provider) => {
   const providerMappings = {
