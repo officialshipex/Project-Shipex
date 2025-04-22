12cadac9e21851a6d605f6287b1f89e02d8c3209
bijaybhanja <bhanjabijayketan@gmail.com>
2025-04-22 14:11:50 +0530

    done

--- backend/routes/adminOrder.router.js
+++ backend/routes/adminOrder.router.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { getOrdersByStatus,searchUser,getAllOrdersByNdrStatus,getAllOrdersByManualRtoStatus } = require("../Admin/order");
+const {getDashboardStats}=require("../Admin/dashboard")
 
 router.get("/adminOrder", getOrdersByStatus);
 router.get("/searchUser",searchUser)
 router.get("/adminNdr",getAllOrdersByNdrStatus)
 router.get("/manualRto",getAllOrdersByManualRtoStatus)
+router.get("/dashboard",getDashboardStats)
 
 module.exports = router;
