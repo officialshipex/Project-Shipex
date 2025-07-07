89c63fed72d3b85b01e91d1fc8eacc2a79dfdd42
bijaybhanja <bhanjabijayketan@gmail.com>
2025-07-07 18:18:40 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const connection = require("./config/database");
-const {startTrackingLoop}=require("./Orders/tracking.controller")
+// const {startTrackingLoop}=require("./Orders/tracking.controller")
 
 console.log("heelo")
 const PORT = process.env.PORT || 5000;
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`✅ Server running on http://65.1.105.160:${PORT}`);
     });
-    startTrackingLoop()
+    // startTrackingLoop()
   } catch (err) {
     console.error("❌ Database connection error:", err);
   }
