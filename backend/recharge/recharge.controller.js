2c6ef235a5e22709f019888992758d4c3042656d
bijaybhanja <bhanjabijayketan@gmail.com>
2025-07-22 19:07:39 +0530

    done

--- backend/recharge/recharge.controller.js
+++ backend/recharge/recharge.controller.js
@@ -7,6 +7,7 @@ const Razorpay = require("razorpay");
 const crypto = require("crypto");
 const cors = require("cors");
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const mongoose = require("mongoose");
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
